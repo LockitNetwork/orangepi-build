aeb81103cf1754338a84b5960a7ec3599c649898
Daniel Strittmatter <daniel@lockitnetwork.com>
2024-09-13 18:47:59 +0200

    Make revision controllable via env var

--- build-vcb2.sh
+++ build-vcb2.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./build.sh 'BUILD_OPT=kernel' 'KERNEL_CONFIGURE=no' 'BOARD=orangepi5b' 'BRANCH=current'
+if [[ -z "$REVISION" ]]; then
+  echo "REVISION is not set"
+  exit 1
+fi
+
+./build.sh 'BUILD_OPT=kernel' 'KERNEL_CONFIGURE=no' 'BOARD=orangepi5b' 'BRANCH=current' "REVISION=$REVISION"
